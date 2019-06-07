f8d962beab0a1d23c6b64aa3149d82a667ed7f2e
eschechter <schechter.evan@gmail.com>
2019-06-07 11:51:32 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(word_list)
+
+        matching_word_hash = self.char_hash(@word)
+
+        word_list.select do |word|
+            self.char_hash(word) == matching_word_hash
+        end
+    end
+
+
+    def char_hash(word)
+        char_hash = {}
+        word.chars.each do |char|
+            if char_hash.keys.include?(char)
+                char_hash[char] += 1
+            else
+                char_hash[char] = 1
+            end
+        end
+        char_hash
+    end
+end
